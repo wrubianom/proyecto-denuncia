f74e22114f6c3c5c91ea10e73c3a61db7bbd62f9
WIlliam Rubiano <WIlliam Rubiano@DESKTOP-9Q1NF0L>
2016-05-08 21:25:31 -0500

    Modificaciones iniciales al nombre de los archivo xhmtl

---
+++ ProyectoDenunciaH/recurso/2. Ingreso parametros 7-05-2016.sql
@@ -0,0 +1,56 @@
+﻿
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'CC','TipoDocumento','Cedula Ciudadania');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'FE','TipoSexo','Femenino');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'MA','TipoSexo','Masculino');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'BOG','LugarNacimiento','Bogota');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'MED','LugarNacimiento','Medellin');
+
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'COM','TipoDelito','Comun');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'HURP','Delito','Hurto a persona');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'SIJ','UnidadInvestigativa','SIJIN');
+
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'FLET','Modalidad','Fleteo');
+
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'FUE','TipoArma','Arma de fuego');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'BLAN','TipoArma','Arma blanca');
+
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'VEH','TipoMovil','Vehiculo');
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'API','TipoMovil','A pie');
