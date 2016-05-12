426b27985a16de2cb91d01f3c875eb6d6dc1387e
user <user@user-PC>
2016-05-11 22:23:16 -0500

    Merge origin/master
    Conflicts:
    	ProyectoDenunciaH/src/java/com/denuncia/ejb/DenunciaDelitoEJBLocal.java

--- ProyectoDenunciaH/recurso/4. Ingreso Parametros 09-05-2016.sql
+++ ProyectoDenunciaH/recurso/4. Ingreso Parametros 09-05-2016.sql
@@ -1,3 +1,8 @@
 ﻿insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
 values
 (nextval('parametro_id_parametro_seq'),'CC','Municipios','Funza');
+
+
+insert into parametro (id_parametro,codigo_parametro,tipo_parametro,nombre_parametro)
+values
+(nextval('parametro_id_parametro_seq'),'CC','Barrio','Madelena');
